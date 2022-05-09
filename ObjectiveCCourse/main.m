dde99d0a6f282ca6d16ba64fb24031cc55a3c0ce
Mark Wong <markwong.digital@gmail.com>
2022-05-09 13:14:27 -0700

    Initial Commit

---
+++ ObjectiveCCourse/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCCourse
+//
+//  Created by Mark Wong on 2022-05-09.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
