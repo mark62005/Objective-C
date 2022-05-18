94f27253b61a7c42b26cb36514727c7b76a90ec7
Mark Wong <markwong.digital@gmail.com>
2022-05-18 13:29:46 -0700

    Add category notes

---
+++ Category/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Category
+//
+//  Created by Mark Wong on 2022-05-18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+    // extension
+  }
+  return 0;
+}
